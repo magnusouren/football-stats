135739166b01be3a985f3237718887fd1012543a
magnusouren <magnut0203@gmail.com>
2024-12-15 22:30:15 +0100

    trigger change

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,7 @@
 export default function About() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">About</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">About:</h1>
+    </div>
+  );
+}
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,7 @@
 export default function Settings() {
   return (
     <div>
-      <h1 className=" font-bold text-2xl mb-16">Settings</h1>
+      <h1 className=" font-bold text-2xl mb-16">Settings:</h1>
     </div>
   );
 }
